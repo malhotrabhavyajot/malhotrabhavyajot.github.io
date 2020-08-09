f5b5088a31b72e552a627de064b7294359307423
Bhavyajot Malhotra <malhotrabhavyajot@gmail.com>
2020-08-09 13:10:31 +0530

    Updates

---
+++ precache-manifest.1a6497dcce09a96346d0c5c7cf76637f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea1513e29e14fd34f3cbec17225b315b",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "1accb0bdc01a515f1034",
+    "url": "/home/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "36286f7408e76967a289",
+    "url": "/home/static/css/main.5f05969b.chunk.css"
+  },
+  {
+    "revision": "1accb0bdc01a515f1034",
+    "url": "/home/static/js/2.92a0caf7.chunk.js"
+  },
+  {
+    "revision": "80fa05aac9f80c71b5f99ecec6ead10d",
+    "url": "/home/static/js/2.92a0caf7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36286f7408e76967a289",
+    "url": "/home/static/js/main.a12ff981.chunk.js"
+  },
+  {
+    "revision": "5c07ecca0456e28e04f0",
+    "url": "/home/static/js/runtime-main.e084aacb.js"
+  },
+  {
+    "revision": "e68353881e978be7f3061e8dbc07792a",
+    "url": "/home/static/media/resume.e6835388.pdf"
+  }
+]);
